f6b88c4fed2442655505b179d3d72bc36f17df60
David Parsons <dave@touchtechpayments.com>
2016-09-28 12:04:29 +0100

    Initial Sample app.

---
+++ Examples/Sample-Swift/Sample-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-Swift
+//
+//  Created by David Parsons on 28/09/2016.
+//  Copyright © 2016 TouchTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
